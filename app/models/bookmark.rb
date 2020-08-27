348745fee579e9baedb4d2e7debfa8ab6186da4a
zalgo <33444773+zalgo-edu@users.noreply.github.com>
2020-08-27 11:08:25 +0900

    #12 change length of memo

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :image, dependent: :destroy, optional: true
 
   validates :title, presence: true, length: { maximum: 255 }
-  validates :memo, presence: true, length: { maximum: 512 }
+  validates :memo, presence: true
   validates :score, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
